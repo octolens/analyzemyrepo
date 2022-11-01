67b6741f4fc4d79c69d89647d467b6ca18fae9e3
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2022-11-01 17:12:36 +0100

    change next links with codemod in components

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -32,11 +32,11 @@ const Header = () => {
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-screen-xl">
           <div className="flex items-center justify-between h-16">
             <div className="flex-1 md:flex md:items-center md:gap-12">
-              <Link href="/">
-                <a className="block text-primary font-bold text-md md:text-xl">
-                  <span className="sr-only">Home</span>
-                  repoanalyzer.com
-                </a>
+              <Link
+                href="/"
+                className="block text-primary font-bold text-md md:text-xl"
+              >
+                <span className="sr-only">Home</span>repoanalyzer.com
               </Link>
             </div>
 
--- src/components/Headers/HeaderDashboard.tsx
+++ src/components/Headers/HeaderDashboard.tsx
@@ -7,11 +7,8 @@ const HeaderDashboard = () => {
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-screen-xl">
           <div className="flex items-center justify-between h-16">
             <div className="flex-1 md:flex md:items-center md:gap-12">
-              <Link href="/">
-                <a className="block text-primary font-bold text-xl">
-                  <span className="sr-only">Home</span>
-                  repoanalyzer.com
-                </a>
+              <Link href="/" className="block text-primary font-bold text-xl">
+                <span className="sr-only">Home</span>repoanalyzer.com
               </Link>
             </div>
           </div>
