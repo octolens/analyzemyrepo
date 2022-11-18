3ef642b16da5af994d5a1c57b89352d7d13f7819
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2022-11-18 15:09:18 +0300

    add apostroph

--- src/components/Cards/TeaseCard.tsx
+++ src/components/Cards/TeaseCard.tsx
@@ -2,8 +2,8 @@ const TeaseCard = () => {
   return (
     <article className="prose">
       <p className="text-center">
-        Unfortunately, we don't have <b>diversity</b> data for this repo yet.
-        You can sign up below and we will collect it for you.
+        Unfortunately, we don&#39;t have <b>diversity</b> data for this repo
+        yet. You can sign up below and we will collect it for you.
       </p>
     </article>
   );
