3bc14a376440201f135698f609874dec8923b0b6
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2022-12-08 11:40:26 +0300

    generating sitemap at a build time

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,23 @@
+import { prisma } from "./src/server/db/client";
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://analyzemyrepo.com",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 7000,
   generateRobotsTxt: true,
-  exclude: ["/server-sitemap.xml"], // <= exclude here
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      "https://analyzemyrepo.com/server-sitemap.xml", // <==== Add here
-    ],
+  additionalPaths: async (config) => {
+    // get all repos from database
+    const repos = await prisma.github_clean_repos.findMany({
+      select: {
+        full_name: true,
+      },
+    });
+
+    return repos.map((repo) => ({
+      loc: `https://analyzemyrepo.com/analyze/${repo.full_name}`,
+      lastmod: new Date().toISOString(),
+    }));
   },
 };
