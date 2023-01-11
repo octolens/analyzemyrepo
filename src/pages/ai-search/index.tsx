65600569ce7acd337f6dfe66ae3e5ddb01f0af67
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2023-01-11 16:34:39 +0400

    feat: improve ai search

--- src/pages/ai-search/index.tsx
+++ src/pages/ai-search/index.tsx
@@ -1,10 +1,21 @@
 import { trpc } from "../../utils/trpc";
 import type { EmbeddingSearchResult } from "../../server/router/embeddings";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { GrStar } from "react-icons/gr";
+import { HiOutlineThumbUp, HiOutlineThumbDown } from "react-icons/hi";
 import Header from "../../components/Headers/NewHeaderSecondary";
+
+const EXAMPLE_QUERIES = [
+  "game engines written in rust",
+  "python fullstack apps framework",
+  "best tools for removing background from images",
+  "js web frameworks",
+  "low code platforms for internal tools",
+  "auth for next.js",
+  "tools to build Figma-like multiplayer apps",
+];
 
 const RepoCard = ({ repo }: { repo: EmbeddingSearchResult }) => {
   const owner = repo.full_name.split("/")[0];
@@ -37,48 +48,116 @@ const RepoCard = ({ repo }: { repo: EmbeddingSearchResult }) => {
 const SearchPage = () => {
   const search = trpc.useMutation("embeddings.search");
   const [query, setQuery] = useState<string>("");
+  const [vote, setVote] = useState<"up" | "down" | null>(null);
+  const [showVote, setShowVote] = useState<boolean>(true);
+
+  const save_vote = trpc.useMutation("embeddings.vote");
+
+  const processVote = async () => {
+    // vote changed, we can save the result
+    if (vote == null) return;
+    setShowVote(false);
+    await save_vote.mutateAsync({ query, vote });
+    setVote(null);
+  };
+
+  useEffect(() => {
+    processVote();
+  }, [vote]);
+
+  useEffect(() => {
+    if (search.isError) {
+      setShowVote(false);
+    } else {
+      setShowVote(true);
+    }
+  }, [search.status]);
 
   return (
     <div className="flex flex-col justify-center align-middle">
       <Header />
-      <h1 className="text-center font-extrabold text-5xl pb-4 pt-48">
+      <h1 className="text-center font-extrabold text-5xl pb-4 pt-28">
         Search GitHub repos using AI
       </h1>
-      <div className="flex flex-col gap-2 items-center">
+      <h2 className="text-center font-semibold text-3xl pb-4">
+        Examples of queries
+      </h2>
+      <div className="text-gray-700 max-w-xs lg:max-w-[800px] flex flex-wrap justify-center gap-2 self-center pb-4">
+        {EXAMPLE_QUERIES.map((query) => (
+          <span
+            className="px-2 bg-primary/10 rounded-lg max-w-xs lg:max-w-[400px] break-words cursor-pointer text-center"
+            key={query}
+            onClick={() => setQuery(query)}
+          >
+            {query}
+          </span>
+        ))}
+      </div>
+      <div className="flex flex-row gap-2 items-center justify-center">
         <input
           type="search"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="AI powered search for GitHub repos"
-          className="w-96 focus:ring-primary focus:border-primary block shadow-sm sm:text-sm border-gray-300 rounded-md"
+          className="w-52 md:w-96 focus:ring-primary focus:border-primary block shadow-sm sm:text-sm border-gray-300 rounded-md"
         />
         <div className="flex flex-row gap-2">
           <button
             disabled={search.isLoading}
             onClick={async () => await search.mutateAsync({ query })}
-            className="bg-primary text-black w-20 rounded-sm"
+            className="bg-primary text-black w-20 h-9 rounded-md font-bold"
           >
             Search
           </button>
           <button
             disabled={search.isLoading}
-            onClick={() => search.reset()}
-            className="w-20 rounded-sm bg-black text-white"
+            onClick={() => {
+              search.reset();
+              setQuery("");
+            }}
+            className="h-9 w-fit text-black"
           >
-            Reset
+            x
           </button>
         </div>
-        <div className="flex py-4">
-          {search.isLoading && "Loading..."}
-          {search.isSuccess && (
-            <div className="grid grid-cols-2 gap-2">
+      </div>
+      <div className="flex py-4 justify-center">
+        {search.isLoading && "Loading..."}
+        {search.isSuccess && (
+          <div className="flex flex-col">
+            <div
+              className={`${
+                showVote ? "" : "hidden "
+              } flex self-center gap-1 items-center transition ease-in-out delay-150 duration-500`}
+            >
+              <p>Do you like the result?</p>
+              <HiOutlineThumbUp
+                size={20}
+                className={`${
+                  vote == "up" ? "bg-primary " : ""
+                } cursor-pointer`}
+                onClick={() => {
+                  setVote("up");
+                }}
+              />
+              <HiOutlineThumbDown
+                size={20}
+                className={`${
+                  vote == "down" ? "bg-primary " : ""
+                } cursor-pointer`}
+                onClick={() => {
+                  setVote("down");
+                }}
+              />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 pt-4">
               {search.data.map((repo) => (
                 <RepoCard repo={repo} />
               ))}
             </div>
-          )}
-          {search.isError && search.error.message}
-        </div>
+          </div>
+        )}
+        {search.isError && search.error.message}
       </div>
     </div>
   );
--- src/server/router/embeddings.ts
+++ src/server/router/embeddings.ts
@@ -10,35 +10,50 @@ export type EmbeddingSearchResult = {
   stargazers_count: number;
 };
 
-export const embeddingsRouter = createRouter().mutation("search", {
-  input: z.object({
-    query: z.string().max(255),
-    limit: z.number().min(1).max(100).default(10),
-  }),
-  async resolve({ input, ctx }): Promise<EmbeddingSearchResult[]> {
-    const url = process.env.EMBEDDINGS_URL as string;
-    const request = await fetch(`${url}/search`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: ("Bearer " + process.env.EMBEDDINGS_TOKEN) as string,
-      },
-      body: JSON.stringify({
-        query: input.query,
-        limit: input.limit,
-      }),
-    });
+export const embeddingsRouter = createRouter()
+  .mutation("search", {
+    input: z.object({
+      query: z.string().max(255),
+      limit: z.number().min(1).max(100).default(10),
+    }),
+    async resolve({ input, ctx }): Promise<EmbeddingSearchResult[]> {
+      const url = process.env.EMBEDDINGS_URL as string;
+      const request = await fetch(`${url}/search`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: ("Bearer " + process.env.EMBEDDINGS_TOKEN) as string,
+        },
+        body: JSON.stringify({
+          query: input.query,
+          limit: input.limit,
+        }),
+      });
 
-    const data = await request.json();
-    return data.map((item: any) => {
-      return {
-        id: item.id as string,
-        score: item.score as number,
-        full_name: item.payload.full_name as string,
-        description: item.payload.description as string,
-        topics: (item.payload.topics as string).split("|"),
-        stargazers_count: item.payload.stargazers_count as number,
-      };
-    });
-  },
-});
+      const data = await request.json();
+      return data.map((item: any) => {
+        return {
+          id: item.id as string,
+          score: item.score as number,
+          full_name: item.payload.full_name as string,
+          description: item.payload.description as string,
+          topics: (item.payload.topics as string).split("|"),
+          stargazers_count: item.payload.stargazers_count as number,
+        };
+      });
+    },
+  })
+  .mutation("vote", {
+    input: z.object({
+      query: z.string(),
+      vote: z.enum(["up", "down"]),
+    }),
+    async resolve({ input, ctx }): Promise<void> {
+      await ctx.prisma.ai_search_feedback.create({
+        data: {
+          query: input.query,
+          vote: input.vote == "up" ? true : false,
+        },
+      });
+    },
+  });
